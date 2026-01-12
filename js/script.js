8d9f44e79f3b365fd0595705eb5d04a59e8ed1f7
Isaac-Loiola <isaackopi@icloud.com>
2026-01-11 23:58:11 -0300

    Flip fixed when cliked in botton audio

--- js/script.js
+++ js/script.js
@@ -7,6 +7,9 @@ style.type = "text/css";
 const frontCard = document.getElementById('card-front');
 const backCard = document.getElementById('card-back');
 
+const player = document.createElement('audio');
+player.src = 'audio/0001.mp3';
+
 frontCard.addEventListener('click', () => {
     style.innerHTML = '.flip-card .flip-card-inner{ transform: rotateY(-180deg);} ';
     document.getElementsByTagName('head')[0].appendChild(style);
@@ -20,18 +23,28 @@ backCard.addEventListener('click', () => {
 
 // second step - add sound when click in button
 
-const player = document.createElement('audio');
-player.src = 'audio/0001.mp3';
-
 const btnAudioFront = document.getElementById('audio-front');
 const btnAudioBack = document.getElementById('audio-back');
 
+const imgCardBack = document.getElementById('img-card-back');
+const imgCardFront = document.getElementById('img-card-front');
+
+
 btnAudioFront.addEventListener('click', () => {
+    event.stopPropagation();
+    imgCardFront.src = 'image/pause-1-svgrepo-com.svg';
     player.play();
 });
+
+player.addEventListener('ended', () => {
+    imgCardFront.src = 'image/play-1003-svgrepo-com (1).svg';
+    imgCardBack.src = 'image/play-1003-svgrepo-com (1).svg';
+})
+
 
 
 btnAudioBack.addEventListener('click', () => {
+    event.stopPropagation();
+    imgCardBack.src = 'image/pause-1-svgrepo-com.svg';
     player.play();
 });
-
