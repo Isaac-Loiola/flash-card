7e27320abdc4dad408bb2ec6af071d1b89d82303
Isaac-Loiola <isaackopi@icloud.com>
2026-01-16 10:55:39 -0300

    Cards Moving

--- js/script.js
+++ js/script.js
@@ -50,3 +50,21 @@ btnAudioBack.addEventListener('click', () => {
     imgCardFront.src = 'image/pause-1-svgrepo-com.svg';
     player.play();
 });
+
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
