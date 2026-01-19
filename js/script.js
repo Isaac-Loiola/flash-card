f0f592a0fb61c915d34ffa5d5fbf6959fbe41301
Isaac-Loiola <isaackopi@icloud.com>
2026-01-19 18:10:48 -0300

    updated functionality of card moving

--- js/script.js
+++ js/script.js
@@ -3,66 +3,83 @@
 const style = document.createElement("style");
 style.type = "text/css";
 
+// Index for card loop animation
+let indexLoop = 0;
 
-const containerBTN = document.getElementById('container-buttons');
-
-const frontCard = document.getElementById('card-front');
-const backCard = document.getElementById('card-back');
-
-const player = document.createElement('audio');
-player.src = 'audio/0001.mp3';
-
-frontCard.addEventListener('click', () => {
+//  front card flip animation 
+function animationFront () {
+    // adding animation class
     style.innerHTML = '.flip-card .flip-card-inner{ transform: rotateY(-180deg);} ';
     document.getElementsByTagName('head')[0].appendChild(style);
+    
+    // showing buttons container.
     containerBTN.hidden = false;
+
+    // display animation
     setTimeout(() => {
         containerBTN.classList.remove('opacity-0')
         containerBTN.classList.add('opacity-100')
     }, 5);
-});
+}
 
-backCard.addEventListener('click', () => {
+function hiddenButtons() {
     setTimeout(() => {
         containerBTN.classList.remove('opacity-100')
         containerBTN.classList.add('opacity-0')
     }, 1);
     containerBTN.hidden = true;
     style.innerHTML = '';
-})
+}
+
+//  back card flip animation 
+function animationBack () {
+    style.innerHTML = '';
+}
+
+const containerCards = document.querySelector('.swiper-wrapper');
+containerCards.addEventListener('click', () =>{
+    const frontCard = event.target.closest('.card-front');
+    const backCard = event.target.closest('.card-back');
+
+    if(frontCard != null){
+        animationFront();
+    }
+    else if(backCard != null){  
+        animationBack();
+    }
+});
+
+// ------ final first step -------
+
+// secund step - next card animation
+
+// the container of the buttons
+const containerBTN = document.getElementById('container-buttons');
+
+// here are the result buttons, too are the animation controller 
+const goodLevel = document.getElementById('ingles-level-good');
+const badLevel = document.getElementById('ingles-level-bad');
+
+function moreOneCountLoop () {
+    indexLoop += 1;
+}
+
+function resetCountLoop () {
+    indexLoop = 0;
+}
+
+goodLevel.addEventListener('click', () => {
+    hiddenButtons();
+    moreOneCountLoop();
+    console.log(indexLoop);
+});
+
+badLevel.addEventListener('click', () => {
+    hiddenButtons();
+    moreOneCountLoop();
+});
 
 // -----------
-
-// second step - add sound when click in button
-
-const btnAudioFront = document.getElementById('audio-front');
-const btnAudioBack = document.getElementById('audio-back');
-
-const imgCardBack = document.getElementById('img-card-back');
-const imgCardFront = document.getElementById('img-card-front');
-
-
-btnAudioFront.addEventListener('click', () => {
-    event.stopPropagation();
-    imgCardBack.src = 'image/pause-1-svgrepo-com.svg';
-    imgCardFront.src = 'image/pause-1-svgrepo-com.svg';
-    player.play();
-});
-
-player.addEventListener('ended', () => {
-    imgCardFront.src = 'image/play-1003-svgrepo-com (1).svg';
-    imgCardBack.src = 'image/play-1003-svgrepo-com (1).svg';
-})
-
-
-
-btnAudioBack.addEventListener('click', () => {
-    event.stopPropagation();
-    imgCardBack.src = 'image/pause-1-svgrepo-com.svg';
-    imgCardFront.src = 'image/pause-1-svgrepo-com.svg';
-    player.play();
-});
-
 
 const swiper = new Swiper('.swiper', {
   // Optional parameters
