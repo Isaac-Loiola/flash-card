7b44d6adaa1d4abf443493f6afaa89c8bba654f0
Isaac-Loiola <isaackopi@icloud.com>
2026-01-20 21:00:40 -0300

    provisional methods added to nota class

--- php/class/nota.php
+++ php/class/nota.php
@@ -12,5 +12,26 @@ class Nota{
     {
         $this->pdo = getConnection();
     }
+
+    function inserir() : bool{
+        $sql = "insert  notas value (:id_card, :nota, :nota_maxima)";
+        $cmd = $this->pdo->prepare($sql);
+        $cmd->bindValue(':id_card', $this->idCard);
+        $cmd->bindValue(':nota', $this->nota);
+        $cmd->bindValue(':nota_maxima', $this->notaMaxima);
+        return $cmd->execute();
+    } 
+
+    function listar() : array{
+        $sql = "select * from notas";
+        $cmd = $this->pdo->prepare($sql);
+        $cmd->execute();
+        $data = $cmd->fetchAll();
+        if(count($data) <= 0){
+            $data = [];
+        }
+        return $data;
+    }
+    
 }
 ?>
